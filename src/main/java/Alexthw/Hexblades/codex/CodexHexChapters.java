8363709639391dcd92274ecc531ddba00e5f7da3
Alexthw46 <77152778+Alexthw46@users.noreply.github.com>
2021-06-01 14:57:37 +0200

    Quick fix

--- src/main/java/Alexthw/Hexblades/codex/CodexHexChapters.java
+++ src/main/java/Alexthw/Hexblades/codex/CodexHexChapters.java
@@ -3,12 +3,10 @@ package Alexthw.Hexblades.codex;
 import Alexthw.Hexblades.registers.HexItem;
 import elucent.eidolon.Registry;
 import elucent.eidolon.codex.*;
-import elucent.eidolon.spell.Sign;
 import elucent.eidolon.spell.Signs;
 import elucent.eidolon.util.ColorUtil;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
-import net.minecraft.util.IItemProvider;
 
 import java.util.List;
 
@@ -30,11 +28,10 @@ public class CodexHexChapters {
     public static void init() {
         CodexChapters Cchapters = null;
         List<Category> Ccategories = getPrivateValue(elucent.eidolon.codex.CodexChapters.class, Cchapters, "categories");
-        ICE_KATANA = new Chapter("hexblades.codex.chapter.ice_katana", new TitlePage("hexblades.codex.page.ice_katana"), new WorktablePage(new ItemStack(Registry.CLEAVING_AXE.get()), new ItemStack((IItemProvider) Registry.PEWTER_INGOT.get()), new ItemStack((IItemProvider) Registry.PEWTER_INGOT.get()), ItemStack.EMPTY, new ItemStack((IItemProvider) Registry.PEWTER_INGOT.get()), new ItemStack(Items.STICK), ItemStack.EMPTY, ItemStack.EMPTY, new ItemStack(Items.STICK), ItemStack.EMPTY, new ItemStack((IItemProvider) Registry.UNHOLY_SYMBOL.get()), ItemStack.EMPTY, new ItemStack((IItemProvider) Registry.PEWTER_INLAY.get()), ItemStack.EMPTY));
-        ;
-        HEX_CONVERSION = new Chapter("hexblades.codex.chapter.hex_touch", new Page[]{new ChantPage("hexblades.codex.page.hex_touch.0", new Sign[]{Signs.MIND_SIGN, Signs.SOUL_SIGN, Signs.MIND_SIGN, Signs.SOUL_SIGN}), new TextPage("hexblades.codex.page.hex_touch.1")});
-        HEXBLADES_INDEX = new Chapter("hexblades.codex.chapter.index", new Page[]{new TitledIndexPage("hexblades.codex.page_index.0", new IndexPage.IndexEntry[]{new IndexPage.IndexEntry(HEX_CONVERSION, new ItemStack((IItemProvider) HexItem.HEXIUM_INGOT.get()))})});
-        Ccategories.add(HEXBLADES = new Category("Hexblades", new ItemStack((IItemProvider) HexItem.PATRON_SOUL.get()), ColorUtil.packColor(255, 255, 255, 255), HEXBLADES_INDEX));
+        ICE_KATANA = new Chapter("hexblades.codex.chapter.ice_katana", new TitlePage("hexblades.codex.page.ice_katana"), new WorktablePage(new ItemStack(Registry.CLEAVING_AXE.get()), new ItemStack(Registry.PEWTER_INGOT.get()), new ItemStack(Registry.PEWTER_INGOT.get()), ItemStack.EMPTY, new ItemStack(Registry.PEWTER_INGOT.get()), new ItemStack(Items.STICK), ItemStack.EMPTY, ItemStack.EMPTY, new ItemStack(Items.STICK), ItemStack.EMPTY, new ItemStack(Registry.UNHOLY_SYMBOL.get()), ItemStack.EMPTY, new ItemStack(Registry.PEWTER_INLAY.get()), ItemStack.EMPTY));
+        HEX_CONVERSION = new Chapter("hexblades.codex.chapter.hex_touch", new ChantPage("hexblades.codex.page.hex_touch.0", Signs.MIND_SIGN, Signs.SOUL_SIGN, Signs.MIND_SIGN, Signs.SOUL_SIGN), new TextPage("hexblades.codex.page.hex_touch.1"));
+        HEXBLADES_INDEX = new Chapter("hexblades.codex.chapter.index", new TitledIndexPage("hexblades.codex.page_index.0", new IndexPage.IndexEntry(HEX_CONVERSION, new ItemStack(HexItem.HEXIUM_INGOT.get())), new IndexPage.IndexEntry(ICE_KATANA, new ItemStack(HexItem.FROST_RAZOR.get()))));
+        Ccategories.add(HEXBLADES = new Category("Hexblades", new ItemStack(HexItem.PATRON_SOUL.get()), ColorUtil.packColor(255, 255, 255, 255), HEXBLADES_INDEX));
 
 
     }
